85876ba4dc8989d3a4648454d1ac28c0969735a9
Hrvoje <hrvojetrcak@gmail.com>
2022-06-30 23:49:43 +0200

    Add option to select avatar image, change button type

--- app/src/main/java/hr/tvz/android/trcak/briftreger/activities/RegisterActivity.kt
+++ app/src/main/java/hr/tvz/android/trcak/briftreger/activities/RegisterActivity.kt
@@ -1,12 +1,23 @@
 package hr.tvz.android.trcak.briftreger.activities
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.graphics.decodeBitmap
 import com.google.firebase.auth.FirebaseAuth
 import hr.tvz.android.trcak.briftreger.databinding.ActivityRegisterBinding
+import javax.xml.transform.Source
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -20,6 +31,7 @@ class RegisterActivity : AppCompatActivity() {
 
         binding.selectPhotoRegistration.setOnClickListener {
             Log.d("MainActivity", "Try to show photo selector")
+            openImageSelector()
         }
 
         binding.registerButtonRegistration.setOnClickListener {
@@ -51,9 +63,42 @@ class RegisterActivity : AppCompatActivity() {
                 if (!it.isSuccessful) return@addOnCompleteListener
 
                 Log.d("MainActivity", "Successfilly created user with uid: ${it.result.user?.uid}")
+                uploadImageToFirebaseStorage()
             }
             .addOnFailureListener {
                 Toast.makeText(this, it.message.toString(), Toast.LENGTH_LONG).show()
             }
+    }
+
+    private fun uploadImageToFirebaseStorage() {
+        // todo
+    }
+
+    private fun openImageSelector() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+
+        resultLauncher.launch(intent)
+    }
+
+    var selectedPhotoUri: Uri?= null
+
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK && it != null) {
+
+            val selectedPhotoUri = it.data?.data
+            Log.d("RegisterActivity", "Photo is selected $selectedPhotoUri")
+
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+            bitmap.setHasAlpha(true)
+            val bitmapDrawable = BitmapDrawable(resources, bitmap)
+
+            /*val bitmap = uri?.let { it1 -> ImageDecoder.createSource(contentResolver, it1) }
+            val bitmapDrawable = Drawable.createFromPath(uri.toString())*/
+
+            binding.selectPhotoRegistration.background = bitmapDrawable
+        } else {
+            Log.d("RegisterActivity", "Photo NOT selected")
+        }
     }
 }
