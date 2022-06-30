5b10b85f3cf78692a95d11a750ceb4efbdc3351b
Hrvoje <hrvojetrcak@gmail.com>
2022-06-30 19:18:21 +0200

    Add View binding to gradle and MainActivity

--- app/src/main/java/hr/tvz/android/trcak/briftreger/MainActivity.kt
+++ app/src/main/java/hr/tvz/android/trcak/briftreger/MainActivity.kt
@@ -2,10 +2,23 @@ package hr.tvz.android.trcak.briftreger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import hr.tvz.android.trcak.briftreger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val email = binding.emailInputRegistration.text.toString()
+        val pass = binding.passwordInputRegistration.text.toString()
+
+        Log.d("MainActivity", "Email is: $email")
+        Log.d("MainActivity", "Email is: $pass")
     }
 }
