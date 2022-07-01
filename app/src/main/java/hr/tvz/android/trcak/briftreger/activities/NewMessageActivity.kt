dab19d704bd5d4ed89876eeffe4aef3e60930694
Hrvoje <hrvojetrcak@gmail.com>
2022-07-01 16:55:31 +0200

    Add list of users to NewMessageActivity

--- app/src/main/java/hr/tvz/android/trcak/briftreger/activities/NewMessageActivity.kt
+++ app/src/main/java/hr/tvz/android/trcak/briftreger/activities/NewMessageActivity.kt
@@ -2,16 +2,24 @@ package hr.tvz.android.trcak.briftreger.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.xwray.groupie.GroupieAdapter
 import com.xwray.groupie.viewbinding.GroupieViewHolder
 import hr.tvz.android.trcak.briftreger.R
 import hr.tvz.android.trcak.briftreger.databinding.ActivityNewMessageBinding
+import hr.tvz.android.trcak.briftreger.models.User
 import hr.tvz.android.trcak.briftreger.models.UserItem
 
 class NewMessageActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityNewMessageBinding
+    private lateinit var binding: ActivityNewMessageBinding
+    private val TAG = "NewMessageActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +29,29 @@ class NewMessageActivity : AppCompatActivity() {
 
         supportActionBar?.title = getString(R.string.select_user)
 
-        val adapter = GroupieAdapter()
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
+        fetchUsers()
 
-        binding.recyclerviewNewmessage.adapter = adapter
+    }
+
+    private fun fetchUsers() {
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val adapter = GroupieAdapter()
+
+                snapshot.children.forEach {
+                    Log.d(TAG, it.toString())
+                    val user = it.getValue(User::class.java)
+                    if (user != null) {
+                        adapter.add(UserItem(user))
+                    }
+                }
+                binding.RVNewMessage.adapter = adapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
     }
 }
--- app/src/main/java/hr/tvz/android/trcak/briftreger/models/UserItem.kt
+++ app/src/main/java/hr/tvz/android/trcak/briftreger/models/UserItem.kt
@@ -1,18 +1,26 @@
 package hr.tvz.android.trcak.briftreger.models
 
-import androidx.recyclerview.widget.RecyclerView
-import com.xwray.groupie.GroupieViewHolder
-import com.xwray.groupie.Item
+import android.view.View
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.viewbinding.BindableItem
 import hr.tvz.android.trcak.briftreger.R
+import hr.tvz.android.trcak.briftreger.databinding.UserRowNewMessageBinding
 
-class UserItem: Item<GroupieViewHolder>() {
+class UserItem(private val user: User): BindableItem<UserRowNewMessageBinding>() {
 
-    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-        // will be called in our list for each user object later on...
+    override fun getLayout() = R.layout.user_row_new_message
+
+    override fun bind(viewBinding: UserRowNewMessageBinding, position: Int) {
+        viewBinding.usernameNewMessage.text = user.username
+
+        viewBinding.imageViewNewMessage.setImageURI(user.profileImageUrl)
+        //Picasso.get().load(user.profileImageUrl).into(viewBinding.imageViewNewMessage)
     }
 
-    override fun getLayout(): Int {
-        return R.layout.user_row_new_message
+    override fun initializeViewBinding(view: View): UserRowNewMessageBinding {
+        return UserRowNewMessageBinding.bind(view)
     }
+
 
 }
