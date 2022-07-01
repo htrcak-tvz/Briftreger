472e210fc3e286b99c63b1a6c055b1879d07b742
Hrvoje <hrvojetrcak@gmail.com>
2022-07-01 12:22:17 +0200

    Add functioning logout button

--- app/src/main/java/hr/tvz/android/trcak/briftreger/activities/LatestMessagesActivity.kt
+++ app/src/main/java/hr/tvz/android/trcak/briftreger/activities/LatestMessagesActivity.kt
@@ -1,12 +1,47 @@
 package hr.tvz.android.trcak.briftreger.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 import hr.tvz.android.trcak.briftreger.R
 
 class LatestMessagesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_messages)
+
+        isUserLoggedIn()
+    }
+
+    private fun isUserLoggedIn() {
+        val uid = FirebaseAuth.getInstance().uid
+        if (uid == null) {
+            loadRegisterActivity()
+        }
+    }
+
+    private fun loadRegisterActivity() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+                loadRegisterActivity()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
