392392abc8eb53f2c50137a5cfce35f7d946d76f
Hrvoje <hrvojetrcak@gmail.com>
2022-07-01 13:50:42 +0200

    Change title on NewMessageActivity

--- app/src/main/java/hr/tvz/android/trcak/briftreger/activities/NewMessageActivity.kt
+++ app/src/main/java/hr/tvz/android/trcak/briftreger/activities/NewMessageActivity.kt
@@ -3,10 +3,18 @@ package hr.tvz.android.trcak.briftreger.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import hr.tvz.android.trcak.briftreger.R
+import hr.tvz.android.trcak.briftreger.databinding.ActivityNewMessageBinding
 
 class NewMessageActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityNewMessageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_message)
+        binding = ActivityNewMessageBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        supportActionBar?.title = getString(R.string.select_user)
     }
 }
